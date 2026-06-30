c7befdee1f03af24c3271af20a3d2e2194571afe
Joseph Aguilar <josepha060308@gmail.com>
2026-06-30 07:40:25 -0500

    Aguilar Joseph, Quico Nicolas, Alessandro Berrocal

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -4,6 +4,9 @@ const Home = () : React.ReactElement => {
 
   return(
       <div className="container">
+        <div style={{ textAlign: "center", padding: "10px", background: "#f0f0f0" }}>
+          <p>Integrantes: Aguilar Joseph, Quico Nicolas, Berrocal Alessandro</p>
+        </div>
         <div className="row">
           <Tabs />
         </div>
